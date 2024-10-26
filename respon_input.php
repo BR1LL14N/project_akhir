2bf6dc05842af870417b413515e5e8aba20d6e49
Brillian Alif <kangnuklir@gmail.com>
2024-10-26 23:04:42 +0700

    Penambahan icon

--- respon_input.php
+++ respon_input.php
@@ -1,14 +1,4 @@
 <?php
-// echo "ini response input";
-// echo "<br>";
-// echo "modul = ".$_GET['modul']."<br>";
-// echo "fitur = ".$_GET['fitur']."<br>";
-// echo "request method = ".$_SERVER['REQUEST_METHOD']."<br>";
-// print_r($_POST);
-// echo "<br>";
-// echo "nama role : ".$_POST['nama_peran']."<br>";
-// echo "keterangan  role : ".$_POST['role_description']."<br>";
-// echo "status role : ".$_POST['role_status']."<br>";
 require_once 'domain-object/node-role.php';
 $roles = [];
 $roles[] = new role(1,$_POST['nama_peran'],$_POST['desc_peran'],$_POST['status_peran'],$_POST['gaji']);
--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -1,11 +1,20 @@
-<div class="w-64 bg-gray-800 text-gray-100 h-screen">
+<div class="w-60 bg-gray-800 text-gray-100 h-screen">
     <div class="p-4 font-bold text-lg">Menu</div>
     <ul class="mt-4 space-y-2">
         <li class="group">
+            <!-- MODUL ROLE DEFAULT SELALU TERIKIR SETIAP SIDEBAR TERKLIK -->
             <a href="index.php?modul=role">
-            <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700">Master Data Role
-            </div>
-        </a>
+                <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700 flex items-center">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.1944 9.07897V7.82129C19.1944 7.2833 18.9779 6.76734 18.5924 6.38692C18.2069 6.0065 17.684 5.79279 17.1389 5.79279H6.8611C6.31593 5.79279 5.79309 6.0065 5.4076 6.38692C5.02211 6.76734 4.80554 7.2833 4.80554 7.82129V9.07897" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M6.86108 5.79276V4.77851C6.86108 4.24051 7.07765 3.72456 7.46314 3.34414C7.84863 2.96372 8.37147 2.75 8.91664 2.75H15.0833C15.6285 2.75 16.1513 2.96372 16.5368 3.34414C16.9223 3.72456 17.1389 4.24051 17.1389 4.77851V5.79276" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M17.92 8.83551H6.08C4.24089 8.83551 2.75 10.3068 2.75 12.1217V17.9638C2.75 19.7787 4.24089 21.25 6.08 21.25H17.92C19.7591 21.25 21.25 19.7787 21.25 17.9638V12.1217C21.25 10.3068 19.7591 8.83551 17.92 8.83551Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M7.88892 12.8925V13.9068C7.88892 14.4448 8.10548 14.9607 8.49097 15.3411C8.87647 15.7216 9.3993 15.9353 9.94447 15.9353H14.0556C14.6008 15.9353 15.1236 15.7216 15.5091 15.3411C15.8946 14.9607 16.1111 14.4448 16.1111 13.9068V12.8925" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    <span class="ml-4">Master Data Role</span>
+                </div>
+            </a>
 <!--            <ul class="ml-4 space-y-1 hidden group-hover:block">-->
 <!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">List</li>-->
 <!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Insert</li>-->
@@ -15,7 +24,16 @@
         </li>
         <li class="group">
             <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700">
-                <a href=#>Master Data User</a>
+                <a href=#>
+                    <div class="flex items-center">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.6177 21.25C19.6177 17.6479 15.6021 14.7206 12 14.7206C8.39794 14.7206 4.38235 17.6479 4.38235 21.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 11.4559C14.404 11.4559 16.3529 9.50701 16.3529 7.10294C16.3529 4.69888 14.404 2.75 12 2.75C9.59594 2.75 7.64706 4.69888 7.64706 7.10294C7.64706 9.50701 9.59594 11.4559 12 11.4559Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+
+                        <span class="ml-4">Master Data User</span>
+                    </div>
+                </a>
             </div>
 <!--            <ul class="ml-4 space-y-1 hidden group-hover:block">-->
 <!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">List</li>-->
@@ -26,17 +44,27 @@
         </li>
         <li class="group">
             <a href="index_barang.php">
-            <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700">Master Data Barang</div>
+                <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700 flex items-center">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.45001 9.23038H17.55C18.0407 9.23038 18.5112 9.4252 18.8582 9.77199C19.2051 10.1188 19.4 10.5891 19.4 11.0796V17.5517C19.4 18.5325 19.0102 19.4732 18.3163 20.1668C17.6224 20.8604 16.6813 21.25 15.7 21.25H8.30001C7.31871 21.25 6.3776 20.8604 5.68371 20.1668C4.98983 19.4732 4.60001 18.5325 4.60001 17.5517V11.0796C4.60001 10.5891 4.79492 10.1188 5.14186 9.77199C5.4888 9.4252 5.95936 9.23038 6.45001 9.23038Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M16.625 11.5531V7.3812C16.6261 6.7737 16.5074 6.17194 16.2756 5.61033C16.0439 5.04872 15.7037 4.53827 15.2745 4.10816C14.8449 3.67764 14.3345 3.33607 13.7726 3.10302C13.2107 2.86996 12.6083 2.75 12 2.75C11.3916 2.75 10.7893 2.86996 10.2274 3.10302C9.66549 3.33607 9.15511 3.67764 8.72549 4.10816C8.29628 4.53827 7.95608 5.04872 7.72435 5.61033C7.49262 6.17194 7.37391 6.7737 7.37499 7.3812L7.37499 11.5531" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    <span class="ml-4">Master Data Barang</span>
+                </div>
             </a>
-<!--            <ul class="ml-4 space-y-1 hidden group-hover:block">-->
-<!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">List</li>-->
-<!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Insert</li>-->
-<!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Update</li>-->
-<!--                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Delete</li>-->
-<!--            </ul>-->
         </li>
         <li class="group">
-            <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700">Menu Transaksi</div>
+            <div class="px-4 py-2 hover:bg-gray-700 cursor-pointer group-hover:bg-gray-700 flex items-center">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21.25 9.94445C21.2482 10.5806 21.0497 11.2006 20.6817 11.7195C20.3137 12.2384 19.7942 12.6307 19.1944 12.8428C18.8662 12.9673 18.5177 13.03 18.1667 13.0278C17.5305 13.026 16.9105 12.8275 16.3916 12.4595C15.8727 12.0915 15.4804 11.572 15.2683 10.9722C15.1438 10.644 15.0811 10.2955 15.0833 9.94445C15.0856 10.2955 15.0228 10.644 14.8983 10.9722C14.6863 11.572 14.2939 12.0915 13.775 12.4595C13.2561 12.8275 12.6361 13.026 12 13.0278C11.3639 13.026 10.7439 12.8275 10.225 12.4595C9.70607 12.0915 9.31371 11.572 9.10167 10.9722C8.97716 10.644 8.91443 10.2955 8.91667 9.94445C8.9189 10.2955 8.85618 10.644 8.73167 10.9722C8.51962 11.572 8.12726 12.0915 7.60837 12.4595C7.08948 12.8275 6.46947 13.026 5.83333 13.0278C5.48226 13.03 5.13381 12.9673 4.80556 12.8428C4.2058 12.6307 3.6863 12.2384 3.3183 11.7195C2.95029 11.2006 2.75179 10.5806 2.75 9.94445L3.2125 8.32057L4.3225 5.15501C4.4633 4.75224 4.72595 4.40326 5.07402 4.15648C5.42209 3.9097 5.83833 3.77735 6.265 3.77779H17.735C18.1617 3.77735 18.5779 3.9097 18.926 4.15648C19.274 4.40326 19.5367 4.75224 19.6775 5.15501L20.7875 8.32057L21.25 9.94445Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M19.1944 12.8428V18.1667C19.1944 18.7118 18.9778 19.2347 18.5923 19.6202C18.2068 20.0057 17.684 20.2222 17.1388 20.2222H6.86105C6.31588 20.2222 5.79304 20.0057 5.40755 19.6202C5.02206 19.2347 4.80549 18.7118 4.80549 18.1667V12.8428" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M8.91666 17.1389H15.0833" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                
+                <span class="ml-4">Menu Transaksi</span>
+                
+            </div>
             <ul class="ml-4 space-y-1 hidden group-hover:block">
                 <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">
                     <a href=#>Insert Transaksi</a>
